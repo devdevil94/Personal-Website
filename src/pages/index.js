f26e77c15c59ddbd0b7ad6b4b3d5f481a82ca997
devdevil-production <saudd2gh@gmail.com>
2020-05-11 12:21:23 +0400

    Create index page

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "./../components/Layout"
+
+export default function Index() {
+  console.log("hello")
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+    </Layout>
+  )
+}
