ec00425efcfd491adef96c790c622cf3b793c6d5
devdevil-production <saudd2gh@gmail.com>
2020-05-18 06:21:47 +0400

    Add top section of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,23 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Hi there!!</h1>
-    </div>
+    <header className="bg-primary">
+      <div className="navbar">
+        <div className="container">
+          <div className="navbar__top ">
+            <label
+              htmlFor="menu-toggle"
+              className="navbar__menuBars text-white rounded"
+            >
+              <i className="fa fa-bars"></i>
+            </label>
+            <div className="navbar__brand text-white">
+              <span>Devdevil</span>
+            </div>
+            <input type="checkbox" id="menu-toggle" />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
