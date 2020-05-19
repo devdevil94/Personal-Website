d4fa23e79c14dc64859859e906aed5c9cb96b335
devdevil-production <saudd2gh@gmail.com>
2020-05-19 10:03:39 +0400

    Minor changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,11 @@
 import React from "react"
 import "../styles/main.css"
+import Navbar from "./Navbar"
 
 export default function Layout({ children }) {
   return (
     <>
-      <header>
-        <div className="text-primary">This is a header</div>
-      </header>
+      <Navbar />
       <main>{children}</main>
       <footer>
         <div>This is a footer</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import Layout from "./../components/Layout"
 
 export default function Index() {
-  console.log("hello")
   return (
     <Layout>
       <h1>Hello World</h1>
