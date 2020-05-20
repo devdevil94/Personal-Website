485e8090219c417aa06e87dda2a75628a1bc6bf6
devdevil-production <saudd2gh@gmail.com>
2020-05-20 16:23:28 +0400

    Add profile section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Layout from "./../components/Layout"
 
 export default function Index() {
+  const [profile, setProfile] = useState({})
+
+  useEffect(() => {
+    const fetchSiteConfig = async () => {
+      const siteData = (await import("../../static/siteConfig.json")).default
+
+      setProfile(siteData)
+    }
+    fetchSiteConfig()
+  }, [])
+
+  console.log(profile)
+  const { author, tagline, extendedBio } = profile
+
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <section className="aboutSection">
+        <div className="container">
+          <h2 className="aboutSection__name text-dark">{author}</h2>
+          <h3 className="aboutSection__tagline text-dark">{tagline}</h3>
+          <p
+            className="aboutSection__bio text-dark"
+            dangerouslySetInnerHTML={{ __html: extendedBio }}
+          ></p>
+        </div>
+      </section>
     </Layout>
   )
 }
