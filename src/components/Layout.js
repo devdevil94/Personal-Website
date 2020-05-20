1bfe9613da58fe9415f9dc9de0f9a4fac5bd5061
devdevil-production <saudd2gh@gmail.com>
2020-05-20 16:17:20 +0400

    Modify layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,18 @@ import Navbar from "./Navbar"
 
 export default function Layout({ children }) {
   return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-      <footer>
-        <div>This is a footer</div>
-      </footer>
-    </>
+    <div className="pageWrapper">
+      <header className="pageWrapper__header bg-primary">
+        <Navbar />
+      </header>
+      <main className="pageWrapper__main">
+        {children}
+        <div className="container">
+          <footer>
+            <div>This is a footer</div>
+          </footer>
+        </div>
+      </main>
+    </div>
   )
 }
