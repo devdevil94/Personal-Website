2efaa34537ed8b3d0182af2d02a82b1aa9a5045e
devdevil-production <saudd2gh@gmail.com>
2020-05-20 16:18:41 +0400

    Minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,98 +24,96 @@ export default function Navbar() {
     }
     fetchSiteConfig()
   }, [])
-  console.log(profile)
+
   return (
-    <header className="bg-primary">
-      <nav className="navbar">
-        <div className="container">
-          <div className="navbar__top">
-            <label
-              htmlFor="menu-toggle"
-              className="navbar__menuBars text-white rounded"
-            >
-              <i className="fa fa-bars"></i>
-            </label>
-            <h1 className="navbar__brand text-white text-center">
-              {profile.brand}
-            </h1>
-          </div>
-          <input type="checkbox" id="menu-toggle" />
-          <div className="navbar__side">
-            <div className="profile">
-              <div className="profile__imgWrapper">
-                <Img
-                  fluid={data.avatar.childImageSharp.fluid}
-                  alt={profile.author}
-                  className="profile__img rounded-full"
-                />
-              </div>
-              <p className="profile__bio text-white text-center">
-                {profile?.bio ?? "Hi there! Welcome to my website"}
-              </p>
-            </div>
-            <ul className="socialList">
-              <li className="socialList__item">
-                <a
-                  href={profile?.accounts?.twitter}
-                  className="rounded-full bg-white text-primary"
-                >
-                  <i className="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li className="socialList__item">
-                <a
-                  href={profile?.accounts?.github}
-                  className="rounded-full bg-white text-primary"
-                >
-                  <i className="fab fa-github-alt"></i>
-                </a>
-              </li>
-              <li className="socialList__item">
-                <a
-                  href={profile?.accounts?.codepen}
-                  className="rounded-full bg-white text-primary"
-                >
-                  <i className="fab fa-codepen"></i>
-                </a>
-              </li>
-            </ul>
-
-            <hr className="navbar__separator" />
-
-            <ul className="navLinks">
-              <li className="navLinks__item">
-                <Link to="/">
-                  <i className="fa fa-user"></i>
-                  About Me
-                </Link>
-              </li>
-              <li className="navLinks__item">
-                <Link to="/blog">
-                  <i className="fa fa-blog"></i>
-                  Blog
-                </Link>
-              </li>
-              <li className="navLinks__item">
-                <Link to="/contact">
-                  <i className="fa fa-phone-alt"></i>
-                  Contact
-                </Link>
-              </li>
-            </ul>
-
-            <hr className="navbar__separator" />
-
-            <a
-              href={profile?.accounts?.upwork}
-              target="_blank"
-              className="navbar__hireBtn text-center text-white rounded"
-            >
-              Hire Me on Upwork
-            </a>
-          </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar__top">
+          <label
+            htmlFor="menu-toggle"
+            className="navbar__menuBars text-white rounded"
+          >
+            <i className="fa fa-bars"></i>
+          </label>
+          <h1 className="navbar__brand text-white text-center">
+            {profile.brand}
+          </h1>
         </div>
-      </nav>
-    </header>
+        <input type="checkbox" id="menu-toggle" />
+        <div className="navbar__side">
+          <div className="profile">
+            <div className="profile__imgWrapper">
+              <Img
+                fluid={data.avatar.childImageSharp.fluid}
+                alt={profile.author}
+                className="profile__img rounded-full"
+              />
+            </div>
+            <p className="profile__bio text-white text-center">
+              {profile?.bio ?? "Hi there! Welcome to my website"}
+            </p>
+          </div>
+          <ul className="socialList">
+            <li className="socialList__item">
+              <a
+                href={profile?.accounts?.twitter}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li className="socialList__item">
+              <a
+                href={profile?.accounts?.github}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-github-alt"></i>
+              </a>
+            </li>
+            <li className="socialList__item">
+              <a
+                href={profile?.accounts?.codepen}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-codepen"></i>
+              </a>
+            </li>
+          </ul>
+
+          <hr className="navbar__separator" />
+
+          <ul className="navLinks">
+            <li className="navLinks__item">
+              <Link to="/">
+                <i className="fa fa-user"></i>
+                About Me
+              </Link>
+            </li>
+            <li className="navLinks__item">
+              <Link to="/blog">
+                <i className="fa fa-blog"></i>
+                Blog
+              </Link>
+            </li>
+            <li className="navLinks__item">
+              <Link to="/contact">
+                <i className="fa fa-phone-alt"></i>
+                Contact
+              </Link>
+            </li>
+          </ul>
+
+          <hr className="navbar__separator" />
+
+          <a
+            href={profile?.accounts?.upwork}
+            target="_blank"
+            className="navbar__hireBtn text-center text-white rounded"
+          >
+            Hire Me on Upwork
+          </a>
+        </div>
+      </div>
+    </nav>
   )
 }
