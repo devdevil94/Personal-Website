c6d8e1094ca1f6adb6f72aca3c5b002284cd1277
devdevil-production <saudd2gh@gmail.com>
2020-05-24 19:53:46 +0400

    Create  blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "./../components/Layout"
+
+export default function BlogPage() {
+  return (
+    <Layout>
+      <h1>This is blog page</h1>
+    </Layout>
+  )
+}
