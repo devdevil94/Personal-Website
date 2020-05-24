cbc6d3cffdd9b42c627ac97b695f509204ab4fd7
devdevil-production <saudd2gh@gmail.com>
2020-05-24 21:32:24 +0400

    Display blog posts cards

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,57 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "./../components/Layout"
+import PostCard from "../components/PostCard"
 
-export default function BlogPage() {
+const BlogPage = ({ data, location }) => {
+  const [page, setPage] = useState(1)
+  const posts = data.allMarkdownRemark.edges
   return (
     <Layout>
-      <h1>This is blog page</h1>
+      <div className="container">
+        <ul className="recentPostsSection__list">
+          {(posts || []).map(({ node: post }) => (
+            <li key={post.id} className="postCard rounded">
+              <PostCard
+                title={post.frontmatter.title}
+                slug={post.fields.slug}
+                date={post.frontmatter.date}
+                excerpt={post.excerpt}
+                fluid={post.frontmatter.img.childImageSharp.fluid}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   )
 }
+
+export const recentPostsQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMM D, YYYY")
+            tags
+            img {
+              childImageSharp {
+                fluid(maxWidth: 600, quality: 90, maxHeight: 380) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default BlogPage
