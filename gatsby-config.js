68a7aa798c641e193eafd9a4489f942eefd345b6
devdevil-production <saudd2gh@gmail.com>
2020-05-26 15:27:10 +0400

    Add remark-prismjs plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,25 @@ module.exports = {
         fonts: [`Roboto`, `Material Icons`, `Oswald`],
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
   ],
 }
