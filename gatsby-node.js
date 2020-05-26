3509b8e44afbb5d4177cd8fed0e0dae813849c91
devdevil-production <saudd2gh@gmail.com>
2020-05-26 12:31:09 +0400

    Create single post pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,3 +14,38 @@ exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const templates = {
+    singlePost: path.resolve("src/templates/SinglePost.js"),
+  }
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors) return Promise.reject(res.errors)
+    const posts = res.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
+      createPage({
+        path: `/blog/${node.fields.slug}`,
+        component: templates.singlePost,
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
+}
