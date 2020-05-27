c279235b94906958f26e57ebbd655971f11b58fb
devdevil-production <saudd2gh@gmail.com>
2020-05-27 14:16:31 +0400

    Add contact form section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,7 +18,7 @@ export default function ContactPage() {
       <section className="contactPage text-dark">
         <div>
           <h1 className="contactPage__title">Contact</h1>
-          <p>
+          <p className="contactPage__info">
             Interested in hiring me for your project or just want to say hi? You
             can fill in the contact form below or send me an email to
             simon.doe@yourwebsite.com Want to get connected? Follow me on the
@@ -51,6 +51,48 @@ export default function ContactPage() {
             </li>
           </ul>
         </div>
+
+        <hr className="rule" />
+
+        <form className="contactForm text-center">
+          <h2 className="contactForm__title">Get In Touch</h2>
+          <div class="inline-fields">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="contactForm__input rounded"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="contactForm__input rounded"
+              required
+            />
+          </div>
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            className="contactForm__input rounded"
+            required
+          />
+          <textarea
+            name="message"
+            rows="10"
+            placeholder="Message"
+            className="contactForm__textarea rounded"
+            required
+          ></textarea>
+          <button
+            type="submit"
+            className="contactForm__submit bg-primary text-white rounded"
+          >
+            Submit
+          </button>
+        </form>
       </section>
     </Layout>
   )
