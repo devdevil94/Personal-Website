0965dadda993c4703adf58a2021bbebbf3135bec
devdevil-production <saudd2gh@gmail.com>
2020-05-27 09:04:40 +0400

    Add social accounts section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,57 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "./../components/Layout"
 
 export default function ContactPage() {
+  const [accounts, setAccounts] = useState({})
+
+  useEffect(() => {
+    const fetchSiteAccounts = async () => {
+      const siteData = (await import("../../static/siteConfig.json")).default
+
+      setAccounts(siteData.accounts)
+    }
+    fetchSiteAccounts()
+  }, [])
+
   return (
     <Layout>
-      <h1>Contact Page</h1>
+      <section className="contactPage text-dark">
+        <div>
+          <h1 className="contactPage__title">Contact</h1>
+          <p>
+            Interested in hiring me for your project or just want to say hi? You
+            can fill in the contact form below or send me an email to
+            simon.doe@yourwebsite.com Want to get connected? Follow me on the
+            social channels below.
+          </p>
+          <ul className="socialList">
+            <li className="socialList__item">
+              <a
+                href={accounts?.twitter}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li className="socialList__item">
+              <a
+                href={accounts?.github}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-github-alt"></i>
+              </a>
+            </li>
+            <li className="socialList__item">
+              <a
+                href={accounts?.codepen}
+                className="rounded-full bg-white text-primary"
+              >
+                <i className="fab fa-codepen"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </Layout>
   )
 }
