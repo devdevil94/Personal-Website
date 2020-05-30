593acf2a4f8a1ec33da6721fbaeb3626a743246c
devdevil-production <saudd2gh@gmail.com>
2020-05-30 17:53:41 +0400

    Add page details

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,16 @@
 import React from "react"
 import Layout from "./../components/Layout"
+import SEO from "../components/SEO"
 
 export default function NotFoundPage() {
   return (
     <Layout>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <SEO title="404" description="This page doesn't exist... Sorry!" />
+      <section className="notFoundPage text-dark">
+        <h1 className="notFoundPage__title">NOT FOUND</h1>
+        <h2 className="notFound__404 text-center">404</h2>
+        <p className="text-center">This page doesn't exist... Sorry!</p>
+      </section>
     </Layout>
   )
 }
