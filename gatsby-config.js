18180587400987cab894ecacdd6c93df9e2b7ef6
devdevil-production <saudd2gh@gmail.com>
2020-05-31 10:21:34 +0400

    Minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/static/images`,
+        path: `${__dirname}/src/images`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/static/posts`,
+        path: `${__dirname}/src/content/posts`,
       },
     },
     {
